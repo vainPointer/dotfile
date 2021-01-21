798b58033913b6053bb21e43dce0400aab0050b2
vainpointer <vainpointer@aliyun.com>
2021-01-21 19:55:18 +0800

    abort fish

--- install.sh
+++ install.sh
@@ -4,12 +4,10 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR=$SOURCE_DIR"/dotconfig"
 LOCAL_DIR="$HOME/.config"
 
-ln -sfv "$CONFIG_DIR/dunst/dunstrc"    "$LOCAL_DIR/dunst/dunstrc"
-ln -sfv "$CONFIG_DIR/fish/config.fish" "$LOCAL_DIR/fish/config.fish"
-ln -sfv "$CONFIG_DIR/i3/config"        "$LOCAL_DIR/i3/config"
-ln -sfv "$CONFIG_DIR/i3status/config"  "$LOCAL_DIR/i3status/config"
-ln -sfv "$CONFIG_DIR/mimeapps.list"    "$LOCAL_DIR/mimeapps.list"
-
+sudo ln -sfv "$SOURCE_DIR/hosts"       "/etc/hosts"
 ln -sfv "$SOURCE_DIR/.bashrc"          "$HOME/.bashrc"
 
-sudo ln -sfv "$SOURCE_DIR/hosts"       "/etc/hosts"
+ln -sfv "$CONFIG_DIR/dunst/dunstrc"    "$LOCAL_DIR/dunst/dunstrc"
+ln -sfv "$CONFIG_DIR/i3/config"        "$LOCAL_DIR/i3/config"
+ln -sfv "$CONFIG_DIR/i3status/config"  "$LOCAL_DIR/i3status/config"
+
