d100eb87cc6a907a691971778d93b2870f03d1cc
vainpointer <vainpointer@aliyun.com>
2021-01-21 12:34:48 +0800

    fix install path

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE_DIR=$SOURCE_DIR"/dotconfig"
+CONFIG_DIR=$SOURCE_DIR"/dotconfig"
 LOCAL_DIR="$HOME/.config"
 
-ln -sfv "$SOURCE_DIR/dunst/dunstrc" "$LOCAL_DIR/dunst/dunstrc"
-ln -sfv "$SOURCE_DIR/fish/config.fish" "$LOCAL_DIR/fish/config.fish"
-ln -sfv "$SOURCE_DIR/i3/config" "$LOCAL_DIR/i3/config"
-ln -sfv "$SOURCE_DIR/i3status/config" "$LOCAL_DIR/i3status"
-ln -sfv "$SOURCE_DIR/mimeapps.list" "$LOCAL_DIR/mimeapps.list"
+ln -sfv "$CONFIG_DIR/dunst/dunstrc"    "$LOCAL_DIR/dunst/dunstrc"
+ln -sfv "$CONFIG_DIR/fish/config.fish" "$LOCAL_DIR/fish/config.fish"
+ln -sfv "$CONFIG_DIR/i3/config"        "$LOCAL_DIR/i3/config"
+ln -sfv "$CONFIG_DIR/i3status/config"  "$LOCAL_DIR/i3status/config"
+ln -sfv "$CONFIG_DIR/mimeapps.list"    "$LOCAL_DIR/mimeapps.list"
